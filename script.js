bdf07f50bd3ffd42696fcb7503b5c5ccbea44803
projects <118543009+AlinaTimofte@users.noreply.github.com>
2026-01-12 23:08:37 +0200

    added new shark, added loading card, tweaked the design

---
+++ script.js
@@ -0,0 +1,43 @@
+const sharks = [
+    {
+        tag: "Tracking",
+        name: "Deep Blue",
+        description: "Is a female greate white shark that is estimated to be 6.1m long or larger and is now 60 years old. She is believed to be one of the largest ever recorded in history.",
+        length: "6.1m",
+        weight: "2000kg",
+        zone: "Pacific",
+        img: "images/White_shark.jpg"
+    },
+    {
+        tag: "Tracking",
+        name: "Ironbound",
+        description: "A tough Great White named after West Ironbound Island. He travels thousands of miles.",
+        length: "3.75m",
+        weight: "539.32kg",
+        zone:"North Atlantic",
+        img: "images/Ironbound.jpg"
+    }
+];
+
+let currentSharkIndex = -1;
+
+function nextShark() {
+    currentSharkIndex = currentSharkIndex + 1;
+
+    if (currentSharkIndex >= sharks.length) {
+        currentSharkIndex = 0;
+    }
+
+    let shark = sharks[currentSharkIndex];
+
+    document.getElementById("tag").innerText = shark.tag;
+    document.getElementById("shark-name").innerText = shark.name;
+    document.getElementById("shark-description").innerText = shark.description;
+    document.getElementById("shark-length").innerText = shark.length;
+    document.getElementById("shark-weight").innerText = shark.weight;
+    document.getElementById("shark-zone").innerText = shark.zone;
+
+    document.getElementById("shark-image").src = shark.img;
+}
+
+nextShark();
