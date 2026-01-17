3df6ca41cd06c2decc01cf8d18a67b5c62b4fdec
projects <118543009+AlinaTimofte@users.noreply.github.com>
2026-01-17 18:58:53 +0200

    inserting a map and testing

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const sharks = [
     {
         tag: "Tracking",
         name: "Deep Blue",
-        description: "Is a female greate white shark that is estimated to be 6.1m long or larger and is now 60 years old. She is believed to be one of the largest ever recorded in history.",
+        description: "Is a female great white shark that is estimated to be 6.1m long or larger and is now 60 years old. She is believed to be one of the largest ever recorded in history.",
         length: "6.1m",
         weight: "2000kg",
         zone: "Pacific",
-        img: "images/White_shark.jpg"
+        img: "images/White_shark.jpg",
+
+        coords: {top: "40%", left: "15%"}
     },
     {
         tag: "Tracking",
@@ -15,7 +17,9 @@ const sharks = [
         length: "3.75m",
         weight: "539.32kg",
         zone:"North Atlantic",
-        img: "images/Ironbound.jpg"
+        img: "images/Ironbound.jpg",
+
+        coords: {top: "25%", left: "32%"}
     }
 ];
 
@@ -38,6 +42,10 @@ function nextShark() {
     document.getElementById("shark-zone").innerText = shark.zone;
 
     document.getElementById("shark-image").src = shark.img;
+
+    let dot = document.getElementById("shark-dot");
+    dot.style.top = shark.coords.top;
+    dot.style.left = shark.coords.left;
 }
 
 nextShark();
